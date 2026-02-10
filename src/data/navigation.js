9af85bb8d8ff07a88f6b5809452a99bc4016568b
wisdom obinna <kcwisdom7@gmail.com>
2026-02-10 10:23:40 -0500

    updated homepage

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,6 +1,6 @@
 export const mainNav = [
   { title: 'Publications', url: '/publications/' },
-  { title: 'AI Art', url: '/aiart/' },
-  { title: 'CEFINI', url: 'https://cefini.com', external: true },
+  { title: 'AI Art competition', url: '/aiart/' },
+  { title: 'Cefini', url: 'https://www.youtube.com/@cefiniHQ', external: true },
   { title: 'Blog', url: '/year-archive/' },
 ];
